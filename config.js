682df251f5bf64c27e774448b35fb403f9a38218
NguyenHoaiKhuong0003 <97690304+NguyenHoaiKhuong0003@users.noreply.github.com>
2023-11-02 22:38:23 +0700

    Add files via upload
    Code 3/11

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Chị Phương Thảo';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Gửi tặng chị';
+const contentCard = 'Chúc chị có một ngày thật vui và hạnh phúc em mong chị không còn buồn hoặc khóc như những lần trước kia em hy vọng sau này em có thể làm nhiều thứ hơn nữa để khiến chị có thể cảm thấy vui em rất thích thấy chị được cười thích nghe giọng nói của chị và nghe chị hát nhưng chắc khoảng thời gian này ai cũng đều bận nhưng không sao, em có vài món quà do tự tay em làm tặng cho chị hy vọng chị sẽ thích, mà em nghĩ không tới được trước sinh nhật chị. Tuổi mới chúc chị luôn tràn đầy năng lượng và gặp những điều tốt trong cuộc sống nhé!!! Happy Anniversary💗💗💗';
+
+const giftImage = 'phuongthao.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
